d3f430d928387040c10893b9b4a4ce61584c4611
wynadiis23 <adii.empty@gmail.com>
2024-09-17 10:19:07 +0800

    chore: update linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
